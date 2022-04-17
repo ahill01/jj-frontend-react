359a6fefd1b18e7fa8d06b323cde6e422af3b335
Allison <amh03160@gmail.com>
2022-04-17 17:48:41 -0500

    added placeholder things from html & vanilla js frontend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,19 @@
 import React from "react"
 function Navbar(){
-    return <h2>Navbar</h2>
+    return(
+        <div>
+        <div className="header">
+        <h1>G.U.S.H.</h1>
+        <h2>Great Underrated Songs, Honey</h2>
+        </div>
+        <div id="navbar" className= "flex-container">
+            <a id ="blog" href="./blog.html">Blog</a>
+            <a id = "playlists" href="./playlists.html">Playlists</a>
+            <a id="faq" href="./faq.html">About/FAQ</a>
+            <a id="submissionPage" href="./submissionPage.html">Submissions</a>
+        </div>
+        </div>
+    )
 }
 
 export default Navbar
--- src/components/blogpost.js
+++ src/components/blogpost.js
@@ -2,7 +2,25 @@ import React from "react";
 function BlogPost(){
 
     return(
-    <p>BLOG POST</p>)
+<div>
+<div id="entry2" className="blogentry">
+        <img src="./images/mp_perfect.jpg" alt="Cover Art of Manequinn Pussy's EP, Perfect"/>
+        <h2><a href ="./darling-mp.html">Darling</a></h2>
+        <h3>Mannequin Pussy</h3>
+        <div className="artistlinks">
+            <a className="bandcamp" href="https://mannequinpussy.bandcamp.com/">bandcamp</a>
+            <a className ="youtube" href ="https://www.youtube.com/watch?v=FT2eaBsNn84&ab_channel=EpitaphRecords">youtube</a>
+            <a className ="spotify" href="https://open.spotify.com/track/4qspFwHsFN9cpkbOrwaR0b?si=348ba69d126e4d3c">spotify</a>
+            <a className ="artistsite" href ="https://mannequinpussy.com/">artist website</a>
+       </div>
+       <div className="writeup">
+           <p>
+            Chasing the tail of Pigs are Pigs, Darling opens with a starkly contrasting 5 seconds of meditative, wave-like static. Quiet, but steadfast octave jumps emerge next, in the form of a guitar flooded in reverb until time becomes almost meaningless. Heartbeat-like drums remind you where the beat falls, like the sound of your own breath in a dark room. It’s almost tempting to describe the track as unlike anything Mannequin Pussy has ever done, ending their Perfect EP in surprisingly delicate and tender territory. Its warmth is more like still-burning-embers rather than the fire and brimstone that dominates the rest of Mannequin Pussy’s discography; however, it has the same lyricism full of brash vulnerability that cuts through everything in its path. The hotblooded reconciliation of pain and the beauty of feeling intensely is reminiscent of Drunk II, as the track dynamically moves through both states simultaneously. Each time the hook hits, it sounds even more like a challenge than a promise. “Darling, I will always defend you” becomes a double edged dare, daring your darling to reciprocate your defense and daring the rest of the world to provide an opportunity to show your devotion. Darling is a beautiful example of what makes Mannequin Pussy’s work so delicously compelling. It’s embracing your feelings with brutal honesty and rebelliously refusing to be ashamed of their intensity. It’s facing forward against whatever’s next, grasping your love’s hand tightly all the way, and proclaiming “I feel fiercely, and I am not afraid”. 
+           </p>
+           <p className ="pubdate">published: 1/13/2022</p>
+       </div>
+        </div>
+        </div>)
 }
 
 export default BlogPost
